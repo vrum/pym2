d3c011171376467eb20ab4ec3a0cc2ad9917b319
bstigurius <bstigurius@1d4ffef8-3049-11df-9f25-831aa717a150>
2010-09-28 22:29:21 +0000

    Little bugfix

--- m2.py
+++ m2.py
@@ -62,9 +62,8 @@ class M2Header:
 		self.cameras        = Chunk()
 		self.camera_lookup  = Chunk()
 		self.ribbon_emitters = Chunk()
-		self.particle_emitters = Chunk()	
-		if(self.modeltype&8):
-			self.unknown = Chunk()
+		self.particle_emitters = Chunk()
+		self.unknown = Chunk()
 			
 	def unpack(self,f):
 		self.magic,         = struct.unpack("i",f.read(4))
